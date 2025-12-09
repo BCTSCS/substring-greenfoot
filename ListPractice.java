2a9b73a2451d001922d4dbf83d17a5aacebcaa02
Dominic <dommos26@bergen.org>
2025-12-08 19:58:37 -0500

    ArrayList-Practice-12/8

---
+++ ListPractice.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class ListPractice {
+public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<>();
+        words.add("syntax");
+        words.add("parser");
+        words.add("token");
+        words.add("lexical");
+    
+    System.out.println("Enhanced For Loop");
+    for(String word : words) {
+        System.out.println(word);
+    }
+
+    System.out.println();
+
+    System.out.println("Traditional for Loop");
+    for(int i=0; i< words.size(); i++) {
+        System.out.println("Index " + i + ": " + words.get(i).toUpperCase());
+    }
+    System.out.println();
+
+    System.out.println("Words with 5+ Letters");
+    for(String word : words){
+        if(word.length()>5){
+            System.out.println(word);
+        }
+    }
+}
+
+
+
+    
+    
+
+
+    
+}
