5e7f8343ae6ada5a2f7715694e9e8cb2c535de3b
Adrian Alvarez <adralv26@bergen.org>
2025-12-11 14:11:10 -0500

    completed socialmedia class

--- poemfolder/SocialMedia.java
+++ poemfolder/SocialMedia.java
@@ -4,16 +4,33 @@ public class SocialMedia {
     private ArrayList<String> posts;
 
     public SocialMedia(String filename) {
-        posts = FileOperator.getStringData(filename);
+        this.posts = FileOperator.getStringData(filename);
 
     }
     public int postsWithLength(int minLength) {
         int count = 0;
-        return count;}
-
-
+        for(String post : posts) {
+            if (post.length() > minLength) {
+                count++;
+            }
+        }
+        return count;
+    }
     public ArrayList<String> postsWithHashtag(String hashtag) {
         ArrayList<String> result = new ArrayList<>();
+
+        for(String post : posts) {
+            if (post.contains(hashtag)) {
+                result.add(post);
+            }
+        }
         return result;
+    }
+    public static void main(String[] args) {
+        SocialMedia jayden = new SocialMedia("somple.txt");
+        System.out.println(jayden.postsWithHashtag("Java"));
+        System.out.println(jayden.postsWithLength(20));
+        // jayden.postsWithHashtag("Java").forEach();
+        // SocialMedia daniel = new SocialMedia("sample1.txt");
     }
 }
